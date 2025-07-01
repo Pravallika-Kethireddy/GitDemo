51cc26ff7fefd1df505f3a8dde388139c46c0f36
Pravallika-Kethireddy <Pravallikareddy.Kethireddy@gmail.com>
2025-07-01 13:17:37 +0530

    new files commit

---
+++ GitDemo/src/Calculator/Countchar.java
@@ -0,0 +1,15 @@
+package Calculator;
+
+public class Countchar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input ="java automation java seleniumjava";
+		String output="java";
+		String[] sp=input.split(output);
+		int count =sp.length-1;
+		System.out.println("count:" +count);
+		
+	}
+
+}
